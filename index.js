2d4a238bae495dcc4b29df602e8db363b28f926d
DavidRMorphew <morphewd@umich.edu>
2021-03-25 09:03:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Add your code here
 const script = document.querySelector("script")
-const ul = document.createElement('ul')
-script.insertAdjacentElement('afterend', ul)
+// const ul = document.createElement('ul')
+// script.insertAdjacentElement('afterend', ul)
+const body = document.body
 
 function submitData(name, email) {
     const data = { "name": name, "email": email };
@@ -14,16 +15,17 @@ function submitData(name, email) {
         },
         body: JSON.stringify(data)
     }
-    fetch(url, configObj)
+    return fetch(url, configObj)
         .then(function(response) { 
             return response.json();
         })
         .then(function(json) {
-            // console.log(json);
-            // document.body.innerHTML = json.id
-            const li = document.createElement('li')
-            li.innerHTML = json.id
-            ul.appendChild(li)
-        });
+            const currentText = body.innerHTML
+            body.innerHTML = currentText + "<br>" + json.id;
+        })
+        .catch(function(error) {
+            const currentText = body.innerHTML
+            body.innerHTML = currentText + "<br>" + error.message;
+        })
         
 }
