1767502e9f61bbb74773873a7ec18e1ca37f9905
DavidRMorphew <morphewd@umich.edu>
2021-03-24 22:13:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const script = document.querySelector("script")
+const ul = document.createElement('ul')
+script.insertAdjacentElement('afterend', ul)
+
+function submitData(name, email) {
+    const data = { "name": name, "email": email };
+    const url = "http://localhost:3000/users"
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    fetch(url, configObj)
+        .then(function(response) { 
+            return response.json();
+        })
+        .then(function(json) {
+            // console.log(json);
+            // document.body.innerHTML = json.id
+            const li = document.createElement('li')
+            li.innerHTML = json.id
+            ul.appendChild(li)
+        });
+        
+}
